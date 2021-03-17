a8d419b507aeee0fc25c61030d6f1583e500e06d
Michele Bonacina <michele.bonacina@gmail.com>
2021-03-17 17:53:11 +0100

    rinominato progetto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('MailManager app is running!');
+    expect(await page.getTitleText()).toEqual('AngularBootstrapTemplate app is running!');
   });
 
   afterEach(async () => {
